73c47491789bdd417d9a17cfa898ef67b62490e5
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-21 15:05:11 +0600

    created api to get specific data with id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +29,22 @@ async function run() {
 
     const cubeCollection = client.db('cubeStore').collection('cubes')
 
-    app.get('/cubes', async (req, res) => {
+    // api to get all data
+    app.get('/all-cubes', async (req, res) => {
       const cursor = cubeCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    // api to get data with specific id
+    app.get('/cubes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cubeCollection.findOne(query);
+      res.send(result);
+    })
+   
+    
 
 
 
